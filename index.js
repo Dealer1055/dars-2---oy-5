a6a8b9261ba2a6f76a30bfa92b098c8acc50b250
Azizbek Nosirov <abb923609@gmail.com>
2024-05-20 10:44:38 +0500

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+let ism = prompt("Ismingizni kiriting : ") 
+let id = prompt("Spece ID ni kiriting : ")
+let pass = prompt("Space Paswordni kiriting : ")
+
+let data = {
+    name: ism,
+    id: id,
+    parol: pass
+};
+
+console.log(data)
+
